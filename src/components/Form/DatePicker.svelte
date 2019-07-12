6f8e7a7fa26ed99d781fc10cd294d8d5d3b28504
Cristian Bustamante <cristian@ketrics.com>
2019-07-12 15:09:07 -0300

    remove console log from datepicker

--- src/components/Form/DatePicker.svelte
+++ src/components/Form/DatePicker.svelte
@@ -42,7 +42,6 @@
 
     function handleWindowClick(e){
         if(visible){
-            console.log(e.target)
             if(!e.target.classList.contains(elementId)){
                 handleClose()
             }
